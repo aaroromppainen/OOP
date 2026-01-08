3ba397629879965215c5d6e16dec313148f58345
aaroromppainen <aaromppa25@students.oamk.fi>
2026-01-08 09:59:52 +0200

    harjoitus

--- VK1/main.cpp
+++ VK1/main.cpp
@@ -2,8 +2,33 @@
 
 using namespace std;
 
+// funktion prototyyppi
+double keskiarvo(double taulukko[4]);
 int main()
 {
-    cout << "Harjoitellaan git ja github" << endl;
+    /*
+    int num = 123;
+    float pi = 3.14;
+    string hello = "Hello Pi:";
+
+    cout << hello << pi << " " << num << endl;
+    cout << "Hello:n pituus " << hello.length() << endl;
+*/
+    double luvut[4] = {8.34, 63.34, 11.0, -5.6};
+    double ka = keskiarvo(luvut);
+    cout << "keskiarvo on " << ka << endl;
+
+    cout << "keskiarvo on " << keskiarvo(luvut) << endl;
     return 0;
+}
+
+double keskiarvo(double taulukko[4])
+{
+    double tulos = 0.0;
+
+    for (int i=0; i < 4; i++){
+        tulos += taulukko[i];
+    }
+    tulos = tulos / 4.0;
+    return tulos;
 }
